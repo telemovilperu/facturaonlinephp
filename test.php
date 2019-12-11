28b3a1786afcc63af859e66623c9f34fbe823f23
Linder Ayala <jemkai84@gmail.com>
2019-12-10 22:02:38 -0500

    Test Obtener CDR-Zip/CDR-XML del comprobante

--- test.php
+++ test.php
@@ -10,55 +10,69 @@ $client->setBaseUrl('https://demoapi2.facturaonline.pe');
 $client->setAccessKey($_ENV['MY_ACCESS_KEY']);
 $client->setSecretKey($_ENV['MY_SECRET_KEY']);
 
+//  * Nota: el $timestamp, tiempo en unix necesario para generar el Signature utilizado para toda comunicacion con la API-FO
 $timestamp = time();
 $client->generateSignature($timestamp);
 
-$client->getSignature();      //  Verificar Signature generado
+//$client->getSignature();      //  Verificar Signature generado
 
-//  * Nota: mantener el $timestamp con el que se genero el Signature para toda comunicacion con la API
-$response = $client->getUsuario($timestamp);        //    verificar conexion con API obteniendo datos de la cuenta
+$jsonString = $client->getUsuario();        //    verificar conexion con API obteniendo datos de la cuenta
 
 $SerieNumero = 'F0011';         //  ConcatenarSerie y numero
 // * Consultar comprobantes segun tipo
-$response= $client->consultaComprobante($SerieNumero, 'factura', $timestamp);
-$response= $client->consultaComprobante($SerieNumero, 'boleta', $timestamp);
-$response= $client->consultaComprobante($SerieNumero, 'notacredito', $timestamp);
-$response= $client->consultaComprobante($SerieNumero, 'notadebito', $timestamp);
-$response= $client->consultaComprobante($SerieNumero, 'guiaremitente', $timestamp);
-
+$jsonString= $client->consultaComprobante($SerieNumero, 'factura');
+$jsonString= $client->consultaComprobante($SerieNumero, 'boleta');
+$jsonString= $client->consultaComprobante($SerieNumero, 'notacredito');
+$jsonString= $client->consultaComprobante($SerieNumero, 'notadebito');
+$jsonString= $client->consultaComprobante($SerieNumero, 'guiaremitente');
 // * Consultar CDR de comprobante en RAW ( informacion en json string)
-$response= $client->CDRComprobante($SerieNumero, 'factura', $timestamp);
-
-//Mostrar respuesta de API en string o array
-$jsonString = $response->getBody();
-$jsonArray  = json_decode($jsonString, true);
-//var_dump($jsonArray);
-
-//echo $client->getUrl();
-$code = $response->getStatusCode(); // 200  *http codes : https://developer.mozilla.org/es/docs/Web/HTTP/Status
-//echo $code;
-
-// test get CDRZip
-$json = $response->getBody();
-$obj = json_decode($json);
-$xml = $obj->xml->data;
-$xml = json_encode($xml);
-$data = trim($xml,'[]');
-$data = explode(',',$data);
-$data = array_map('chr',$data);
-$xml = implode('',$data);
-$decoded = base64_decode($xml);
-$file = 'CDR.zip';
-file_put_contents($file, $decoded);
-if (file_exists($file)) {
+$jsonString= $client->CDRComprobante($SerieNumero, 'factura');
+// * Obtener CDR en ZIP, respuesta  de sunat
+$jsonArray = json_decode($jsonString, true); //	array de respuesta $client->CDRComprobante(SerieNumero, TipoComprobante);
+$nombreZip = 'CDR-'.$jsonArray['idEmisor'].'-'.$jsonArray['serie'].$jsonArray['numero'].'.zip';
+$rawXML	   = base64_decode($jsonArray['xml']);
+file_put_contents($nombreZip, $rawXML);
+if (file_exists($nombreZip)) {
+// * Descomentar para Test de Descargar Zip
+/*
+ * 
 	header('Content-Description: File Transfer');
 	header('Content-Type: application/octet-stream');
-	header('Content-Disposition: attachment; filename="'.basename($file).'"');
+	header('Content-Disposition: attachment; filename="'.basename($nombreZip).'"');
 	header('Expires: 0');
 	header('Cache-Control: must-revalidate');
 	header('Pragma: public');
-	header('Content-Length: ' . filesize($file));
-	readfile($file);
+	header('Content-Length: ' . filesize($nombreZip));
+	readfile($nombreZip);
 	exit;
+ *
+ */
+// test read CDRZip get original XML-CDR
+	$zip = new ZipArchive;
+	if ($zip->open($nombreZip) === TRUE) {
+		$cdrXML = $zip->getFromIndex(1);
+		$zip->close();
+		// Force download XML
+		/*
+		header('Content-disposition: attachment; filename=CDR.xml');
+		header ("Content-Type:text/xml"); 
+		print($cdrXML);
+		header("Expires: 0");
+		exit;
+		*/
+	} else {
+		echo 'failed';
+	}
+	unlink($nombreZip);
+//	exit;
 }
-exit; 
+
+//Mostrar respuesta de API en string JSON
+//header('Content-Type: application/json');
+print($jsonString);
+// Convertir respuesta a Array asociativo PHP
+$jsonArray = json_decode($jsonString, true);
+echo "<pre>";
+print_r($jsonArray);
+echo "</pre>";
+exit;
